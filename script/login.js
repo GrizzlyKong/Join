559551ad31808edf9fc02df05b9878a3b76ef697
nckPavlov <150789441+nckPavlov@users.noreply.github.com>
2023-12-18 11:37:31 +0100

    updated sign up

--- script/login.js
+++ script/login.js
@@ -1,12 +1,12 @@
-const STORAGE_TOKEN = 'NBZULW333XMNBK2G8I6XU4NQSQSP4I72EP4JOU73';
+/* const STORAGE_TOKEN = 'UK3WMTJPY9HCOS9AB0PGAT5U9XL1Y2BKP4MIYIVD';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
-
+ */
 async function init() {
   displayHidden();
   logoAnimation();
 }
 
-async function setItem(key, value) {
+/* async function setItem(key, value) {
     const payload = { key, value, token: STORAGE_TOKEN }
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
     .then(res => res.json());
@@ -15,7 +15,7 @@ async function setItem(key, value) {
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
     return fetch(url).then(res => res.json()).then(res => res.data.value);
-}
+} */
 
 function logoAnimation() {
   const logo = document.getElementById("logo-animated");
--- script/sign-up.js
+++ script/sign-up.js
@@ -1,4 +1,4 @@
-const STORAGE_TOKEN = 'NBZULW333XMNBK2G8I6XU4NQSQSP4I72EP4JOU73';
+const STORAGE_TOKEN = 'UK3WMTJPY9HCOS9AB0PGAT5U9XL1Y2BKP4MIYIVD';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 let users = [];
 
@@ -6,34 +6,41 @@ async function init() {
     loadUsers();
 }
 
-async function loadUsers() {
-    setusers = await setItem('users');
-    getusers = await getItem('users');
+async function loadUsers(){
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch(e){
+        console.error('Loading error:', e);
+    }
 }
 
 async function register() {
     registerBtn.disabled = true;
     users.push({
         email: email.value,
-        password: password.value
+        password: password.value,
     });
-
-    setItem('users', JSON.stringify(users));
-    resetForm(registerBtn);
+    await setItem('users', JSON.stringify(users));
+    resetForm();
 }
-function resetForm(registerBtn) {
+
+function resetForm() {
     email.value = '';
     password.value = '';
     registerBtn.disabled = false;
 }
-
 async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN }
+    const payload = { key, value, token: STORAGE_TOKEN };
     return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-    .then(res => res.json());
+        .then(res => res.json());
 }
 
 async function getItem(key) {
     const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => res.data.value);
+    return fetch(url).then(res => res.json()).then(res => {
+        // Verbesserter code
+        if (res.data) { 
+            return res.data.value;
+        } throw `Could not find data with key "${key}".`;
+    });
 }
