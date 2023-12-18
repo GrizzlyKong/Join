589006fe1ec64e331da23537b940b9e42791e9ba
nckPavlov <150789441+nckPavlov@users.noreply.github.com>
2023-12-18 11:53:36 +0100

    nothing

--- script/sign-up.js
+++ script/sign-up.js
@@ -1,8 +1,7 @@
-const STORAGE_TOKEN = 'UK3WMTJPY9HCOS9AB0PGAT5U9XL1Y2BKP4MIYIVD';
-const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
 let users = [];
 
-async function init() {
+
+async function init(){
     loadUsers();
 }
 
@@ -13,6 +12,7 @@ async function loadUsers(){
         console.error('Loading error:', e);
     }
 }
+
 
 async function register() {
     registerBtn.disabled = true;
@@ -28,19 +28,4 @@ function resetForm() {
     email.value = '';
     password.value = '';
     registerBtn.disabled = false;
-}
-async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN };
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-        .then(res => res.json());
-}
-
-async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => {
-        // Verbesserter code
-        if (res.data) { 
-            return res.data.value;
-        } throw `Could not find data with key "${key}".`;
-    });
 }
