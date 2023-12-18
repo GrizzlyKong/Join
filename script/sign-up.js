351243a809c630e711291cbf601cdfe793e198f2
nckPavlov <150789441+nckPavlov@users.noreply.github.com>
2023-12-18 14:00:43 +0100

    nothing

--- script/sign-up.js
+++ script/sign-up.js
@@ -15,17 +15,25 @@ async function loadUsers(){
 
 
 async function register() {
+    let name1 = document.getElementById('name1');
+    let email1 = document.getElementById('email1');
+    let password1 = document.getElementById('password1');
+    let passwordConfirm = document.getElementById('passwordConfirm');
     registerBtn.disabled = true;
     users.push({
-        email: email.value,
-        password: password.value,
+        name: name1.value,
+        email: email1.value,
+        password: password1.value,
+        passwordConfirm: passwordConfirm.value,
     });
     await setItem('users', JSON.stringify(users));
     resetForm();
 }
 
 function resetForm() {
-    email.value = '';
-    password.value = '';
+    name1.value = '';
+    email1.value = '';
+    password1.value = '';
+    passwordConfirm.value = '';
     registerBtn.disabled = false;
 }
