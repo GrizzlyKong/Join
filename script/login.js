34e6583ecc7bed813c84560063c1bc20bd25f252
nckPavlov <150789441+nckPavlov@users.noreply.github.com>
2023-12-20 07:27:56 +0100

    added sign up and log in

--- script/login.js
+++ script/login.js
@@ -1,21 +1,28 @@
-/* const STORAGE_TOKEN = 'UK3WMTJPY9HCOS9AB0PGAT5U9XL1Y2BKP4MIYIVD';
+const STORAGE_TOKEN = 'UK3WMTJPY9HCOS9AB0PGAT5U9XL1Y2BKP4MIYIVD';
 const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
- */
+
+let users = [];
+
 async function init() {
   displayHidden();
   logoAnimation();
 }
 
-/* async function setItem(key, value) {
-    const payload = { key, value, token: STORAGE_TOKEN }
-    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
-    .then(res => res.json());
+async function setItem(key, value) {
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
+      .then(res => res.json());
 }
 
 async function getItem(key) {
-    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
-    return fetch(url).then(res => res.json()).then(res => res.data.value);
-} */
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  return fetch(url).then(res => res.json()).then(res => {
+      // Verbesserter code
+      if (res.data) { 
+          return res.data.value;
+      } throw `Could not find data with key "${key}".`;
+  });
+}
 
 function logoAnimation() {
   const logo = document.getElementById("logo-animated");
@@ -33,4 +40,30 @@ function displayHidden() {
     hidden2.style.opacity = 1;
     hidden3.style.opacity = 1;
   }, 1500);
+  document.getElementById('logo-anime').classList.add('zIndex');
+  document.getElementById('log-in-content-id').classList.add('zIndex2');
 }
+
+async function register() {
+  let email1 = document.getElementById("email");
+  let password = document.getElementById("password");
+  registerBtn2.disabled = true;
+  pushUsers();
+  await setItem("users", JSON.stringify(users));
+  resetForm();
+/*   location.replace("../html/summary.html"); */
+}
+
+function pushUsers() {
+  users.push({
+    email: email.value,
+    password: password.value,
+  });
+}
+
+function resetForm() {
+  email.value = "";
+  password.value = "";
+  registerBtn2.disabled = false;
+}
+
--- script/sign-up.js
+++ script/sign-up.js
@@ -1,39 +1,55 @@
 let users = [];
 
-
-async function init(){
-    loadUsers();
+async function init() {
+  loadUsers();
 }
 
-async function loadUsers(){
-    try {
-        users = JSON.parse(await getItem('users'));
-    } catch(e){
-        console.error('Loading error:', e);
-    }
+async function loadUsers() {
+  try {
+    users = JSON.parse(await getItem("users"));
+  } catch (e) {
+    console.error("Loading error:", e);
+  }
 }
-
 
 async function register() {
-    let name1 = document.getElementById('name1');
-    let email1 = document.getElementById('email1');
-    let password1 = document.getElementById('password1');
-    let passwordConfirm = document.getElementById('passwordConfirm');
-    registerBtn.disabled = true;
-    users.push({
-        name: name1.value,
-        email: email1.value,
-        password: password1.value,
-        passwordConfirm: passwordConfirm.value,
-    });
-    await setItem('users', JSON.stringify(users));
-    resetForm();
+  let name1 = document.getElementById("name1");
+  let email1 = document.getElementById("email1");
+  let password1 = document.getElementById("password1");
+  let passwordConfirm = document.getElementById("passwordConfirm");
+  registerBtn.disabled = true;
+  pushUsers();
+  await setItem("users", JSON.stringify(users));
+  resetForm();
+  SignedUpSuccessfully();
+}
+
+function SignedUpSuccessfully() {
+  document.getElementById("sign-up-id").classList.remove("d-none");
+  document.getElementById("sign-up-id").classList.add("sign-up-animation");
+  document.getElementById("body").classList.add("body");
+  document.getElementById("sign-up-content-id").classList.add("blur");
+  setTimeout(() => {
+    document.getElementById("sign-up-id").classList.add("d-none");
+    document.getElementById("body").classList.remove("body");
+    document.getElementById("sign-up-content-id").classList.remove("blur");
+    location.replace("../html/login.html");
+  }, 3000);
+}
+
+function pushUsers() {
+  users.push({
+    name: name1.value,
+    email: email1.value,
+    password: password1.value,
+    passwordConfirm: passwordConfirm.value,
+  });
 }
 
 function resetForm() {
-    name1.value = '';
-    email1.value = '';
-    password1.value = '';
-    passwordConfirm.value = '';
-    registerBtn.disabled = false;
+  name1.value = "";
+  email1.value = "";
+  password1.value = "";
+  passwordConfirm.value = "";
+  registerBtn.disabled = false;
 }
