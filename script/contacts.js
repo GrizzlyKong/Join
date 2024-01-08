96581912c4b0389bbdd0f6c72b468c798dcc01c8
Andrej Vasas <150653451+GrizzlyKong@users.noreply.github.com>
2024-01-08 15:35:19 +0100

    Changes to be committed:   contacts page 80% finished 	modified:   html/contacts.html 	modified:   html/login.html 	modified:   script/contacts.js

--- script/contacts.js
+++ script/contacts.js
@@ -1,13 +1,13 @@
 async function init() {
-await includeHTML();
+  await includeHTML();
+  displayUserContacts();
 }
-
 
 async function includeHTML() {
   let includeElements = document.querySelectorAll("[w3-include-html]");
   for (let i = 0; i < includeElements.length; i++) {
     const element = includeElements[i];
-    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    file = element.getAttribute("w3-include-html");
     let resp = await fetch(file);
     if (resp.ok) {
       element.innerHTML = await resp.text();
@@ -17,55 +17,52 @@ async function includeHTML() {
   }
 }
 
-
 function addNewContact() {
   let newContact = document.getElementById("add-new-contact");
   document.getElementById("contacts-div").classList.add("background");
   document.getElementById("add-new-contact").classList.add("sign-up-animation");
   document.getElementById("add-new-contact").classList.remove("d-none");
-  newContact.innerHTML = /*HTML*/ ` 
-  <div id="add-new-contact-id" class="addNewContactDiv">
-    <div class ="left-side-add-contact column">
-      <div><img src="/assets/icons/logo.svg"></div>
-      <h1>Add contact</h1>
-      <span>Tasks are better with a team!</span>
-      <div class="line"></div>
-    </div>
-    <div class = "right-side-add-contact">
-      <img onclick="closeAddContact()" class="close absolute pointer" src="/assets/icons/close.svg">
-      <div class = "account center">
-        <div class="adding-contact-icon"><img src="/assets/icons/person.png"></div>
+  newContact.innerHTML = `
+    <div id="add-new-contact-id" class="addNewContactDiv">
+      <div class ="left-side-add-contact column">
+        <div><img src="/assets/icons/logo.svg"></div>
+        <h1>Add contact</h1>
+        <span>Tasks are better with a team!</span>
+        <div class="line"></div>
       </div>
-      <div>
-        <form onsubmit="return false;">
-          <div class="form-contacs">
-            <div class="center"><input id="contactNameInput" class="log-in-field column center pointer" required type="text" placeholder="Name"><img
-              class="log-in-mail-lock-icon" src="../assets/icons/person-small.png"></div>
-            <div class="center"><input id="contactEmailInput" class="log-in-field column center pointer" required type="email" placeholder="Email"><img
-              class="log-in-mail-lock-icon" src="../assets/icons/mail.png"></div>
-            <div class="center"><input id="contactPhoneInput" class="log-in-field column center pointer" required type="number" placeholder="Phone"><img
-              class="log-in-mail-lock-icon" src="../assets/icons/call.png"></div>
-          </div>
-          <div class="right-bottom">
-            <div class="clear-and-create-task">
-              <div class="clear pointer center" onclick="clearInputAddingContact()">
-                <span>Clear</span>
-                <img class="cancel1" src="/assets/icons/cancel.svg" alt="">
-                <img class="cancel2 d-none" src="/assets/icons/cancel2.svg" alt="">
-              </div>
-              <div class="create-task pointer center" onclick="addingContact()">
-                <span>Create contact</span>
-                <img src="/assets/icons/check.svg" alt="">
+      <div class = "right-side-add-contact">
+        <img onclick="closeAddContact()" class="close absolute pointer" src="/assets/icons/close.svg">
+        <div class = "account center">
+          <div class="adding-contact-icon"><img src="/assets/icons/person.png"></div>
+        </div>
+        <div>
+          <form onsubmit="return false;">
+            <div class="form-contacs">
+              <div class="center"><input id="contactNameInput" class="log-in-field column center pointer" required type="text" placeholder="Name"><img
+                class="log-in-mail-lock-icon" src="../assets/icons/person-small.png"></div>
+              <div class="center"><input id="contactEmailInput" class="log-in-field column center pointer" required type="email" placeholder="Email"><img
+                class="log-in-mail-lock-icon" src="../assets/icons/mail.png"></div>
+              <div class="center"><input id="contactPhoneInput" class="log-in-field column center pointer" required type="number" placeholder="Phone"><img
+                class="log-in-mail-lock-icon" src="../assets/icons/call.png"></div>
+            </div>
+            <div class="right-bottom">
+              <div class="clear-and-create-task">
+                <div class="clear pointer center" onclick="clearInputAddingContact()">
+                  <span>Clear</span>
+                  <img class="cancel1" src="/assets/icons/cancel.svg" alt="">
+                  <img class="cancel2 d-none" src="/assets/icons/cancel2.svg" alt="">
+                </div>
+                <div class="create-task pointer center" onclick="addingContact()">
+                  <span>Create contact</span>
+                  <img src="/assets/icons/check.svg" alt="">
+                </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
   `;
-
-
   const newContactElement = document.createElement("div");
   newContactElement.className = "added-contact pointer";
   newContactElement.onclick = showContact;
@@ -80,13 +77,11 @@ function addNewContact() {
   `;
 }
 
-
 function clearInputAddingContact() {
   document.getElementById("contactNameInput").value = "";
   document.getElementById("contactEmailInput").value = "";
   document.getElementById("contactPhoneInput").value = "";
 }
-
 
 function insertAfter(newNode, referenceNode) {
   var nextSibling = referenceNode.nextSibling;
@@ -97,16 +92,14 @@ function insertAfter(newNode, referenceNode) {
   }
 }
 
-
 function getRandomColor() {
-const letters = "0123456789ABCDEF";
-let color = "#";
-for (let i = 0; i < 6; i++) {
-  color += letters[Math.floor(Math.random() * 16)];
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
-return color;
-}
-
 
 function addingContact() {
   const name = document.getElementById("contactNameInput").value;
@@ -114,12 +107,11 @@ function addingContact() {
   const phone = document.getElementById("contactPhoneInput").value;
 
   if (!name || !email || !phone) {
-      alert("Please fill in all fields before creating a contact.");
-      return;
+    alert("Please fill in all fields before creating a contact.");
+    return;
   }
 
   const initialLetter = name.charAt(0).toUpperCase();
-
   const newContactElement = document.createElement("div");
   newContactElement.className = "added-contact pointer";
   newContactElement.onclick = showContact;
@@ -127,25 +119,38 @@ function addingContact() {
   const randomColor = getRandomColor();
 
   newContactElement.innerHTML = `
-  <div class="primary-contact-icon-container">
-      <div class="added-contact-icon" style="background-color: ${randomColor} !important; border: 4px solid white;">${initialLetter}</div>
-  </div>
-  <div class="moveRight">
-      <p>${name}</p>
-      <a class="contact-link">${email}</a>
-  </div>
-`;
+    <div class="primary-contact-icon-container">
+        <div class="added-contact-icon" style="background-color: ${randomColor} !important; border: 4px solid white;">${initialLetter}</div>
+    </div>
+    <div class="moveRight">
+        <p>${name}</p>
+        <a class="contact-link">${email}</a>
+    </div>
+  `;
 
   const contactsMenu = document.getElementById("contactsMenu");
-  const referenceNode = contactsMenu.childNodes[2]; // Reference node at the third position
+  const referenceNode = contactsMenu.childNodes[2];
   insertAfter(newContactElement, referenceNode);
 
   updateLetterContacts(initialLetter);
   clearInputAddingContact();
   closeAddContact();
+  saveContactToLocalStorage({ name, email, phone, color: randomColor });
 }
 
+function saveContactToLocalStorage(contact) {
+  const loggedInUserName = localStorage.getItem("loggedInUserName");
 
+  if (!loggedInUserName) {
+    console.error("No logged-in user found. Contact cannot be saved.");
+    return;
+  }
+
+  const userContactsKey = `contacts_${loggedInUserName}`;
+  const existingContacts = JSON.parse(localStorage.getItem(userContactsKey)) || [];
+  existingContacts.push(contact);
+  localStorage.setItem(userContactsKey, JSON.stringify(existingContacts));
+}
 
 function updateLetterContacts(firstLetter) {
   const contactsMenu = document.getElementById("contactsMenu");
@@ -166,14 +171,120 @@ function updateLetterContacts(firstLetter) {
   }
 }
 
-
 function closeAddContact() {
   document.getElementById("add-new-contact").classList.add("d-none");
   document.getElementById("add-new-contact").classList.add("sign-up-animation-close");
 }
 
-
 function showContact() {
-  document.getElementById("contact-info").style.display = "flex";
-  document.getElementById("contact-info").classList.add("show-contact-animation");
+  const contactInfoDiv = document.getElementById("contact-info");
+  const contactInfoName = document.querySelector(".contact-info-name");
+  const contactInfoIcon = document.querySelector(".contact-info-icon");
+  const contactInfoLink = document.querySelector(".contact-link");
+  const contactInfoDetails = document.querySelector(".contact-information-details span");
+
+  const name = this.querySelector(".moveRight p").innerText;
+  const email = this.querySelector(".moveRight a").innerText;
+  const initialLetter = this.querySelector(".added-contact-icon").innerText;
+  const color = this.querySelector(".added-contact-icon").style.backgroundColor;
+  const phoneNumber = this.getAttribute("data-phone-number");
+
+  contactInfoName.innerText = name;
+  contactInfoIcon.innerText = initialLetter;
+  contactInfoIcon.style.backgroundColor = color;
+  contactInfoLink.innerText = email;
+  contactInfoDetails.innerHTML = `
+    <p>Email</p>
+    <a class="contact-link">${email}</a>
+    <p>Phone</p>
+    <span>${phoneNumber}</span>
+  `;
+
+  contactInfoDiv.style.display = "flex";
+  contactInfoDiv.classList.add("show-contact-animation");
+}
+
+function displayUserContacts() {
+  const loggedInUserName = localStorage.getItem("loggedInUserName");
+
+  if (!loggedInUserName) {
+    console.error("No logged-in user found. Contacts cannot be displayed.");
+    return;
+  }
+
+  const userContactsKey = `contacts_${loggedInUserName}`;
+  const savedContacts = JSON.parse(localStorage.getItem(userContactsKey)) || [];
+
+  savedContacts.forEach((contact, index) => {
+    const { name, email, phone, color } = contact;
+    const initialLetter = name.charAt(0).toUpperCase();
+
+    const newContactElement = document.createElement("div");
+    newContactElement.className = "added-contact pointer";
+    newContactElement.onclick = showContact;
+    newContactElement.setAttribute("data-phone-number", phone);
+    newContactElement.innerHTML = `
+      <div class="primary-contact-icon-container">
+        <div class="added-contact-icon" style="background-color: ${color} !important; border: 4px solid white;">${initialLetter}</div>
+      </div>
+      <div class="moveRight">
+        <p>${name}</p>
+        <a class="contact-link">${email}</a>
+      </div>
+    `;
+
+    const contactsMenu = document.getElementById("contactsMenu");
+    const referenceNode = contactsMenu.childNodes[2];
+    insertAfter(newContactElement, referenceNode);
+
+    updateLetterContacts(initialLetter);
+  });
+}
+
+
+function deleteContact() {
+  const contactInfoDiv = document.getElementById("contact-info");
+  const contactName = document.querySelector(".contact-info-name").innerText;
+
+  // Remove the contact from the page
+  const contactsLayout = document.getElementById("contactsLayout");
+  const contactToDelete = Array.from(contactsLayout.getElementsByClassName("added-contact"))
+    .find(contact => contact.querySelector(".moveRight p").innerText === contactName);
+
+  if (contactToDelete) {
+    contactsLayout.removeChild(contactToDelete);
+  }
+
+  // Remove the contact from local storage
+  const loggedInUserName = localStorage.getItem("loggedInUserName");
+
+  if (!loggedInUserName) {
+    console.error("No logged-in user found. Contact cannot be deleted from local storage.");
+    return;
+  }
+
+  const userContactsKey = `contacts_${loggedInUserName}`;
+  let existingContacts = JSON.parse(localStorage.getItem(userContactsKey)) || [];
+
+  // Find the index of the contact to delete
+  const contactIndex = existingContacts.findIndex(contact => contact.name === contactName);
+
+  if (contactIndex !== -1) {
+    existingContacts.splice(contactIndex, 1);
+    localStorage.setItem(userContactsKey, JSON.stringify(existingContacts));
+  }
+
+  // Hide the contact info div
+  contactInfoDiv.style.display = "none";
+
+  // Reload the page
+  location.reload();
+}
+
+
+init();
+
+
+function editContact() {
+  
 }
